f3de4549ecda6535973902d8baca4b52c9911ed5
Abdullah <abdullah981066@gmail.com>
2024-12-25 17:08:02 +0530

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,20 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +28,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  "plugin:prettier/recommended", // Add this to enable Prettier integration
+  "prettier", // This disables conflicting rules
 ];
 
 export default eslintConfig;
