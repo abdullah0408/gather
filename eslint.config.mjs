8c61ada9be125496231ab9804fa750d395474210
Abdullah <abdullah981066@gmail.com>
2024-12-27 06:58:26 +0530

    "testing"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,3 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
-
-// export default eslintConfig;
-
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -28,8 +11,25 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  "plugin:prettier/recommended", // Add this to enable Prettier integration
-  "prettier", // This disables conflicting rules
 ];
 
 export default eslintConfig;
+
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   "plugin:prettier/recommended", // Add this to enable Prettier integration
+//   "prettier", // This disables conflicting rules
+// ];
+
+// export default eslintConfig;
