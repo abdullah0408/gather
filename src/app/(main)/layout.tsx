ac25df9f7f97695f3e6da53a6f8c8e2aa66f6183
abdullah <abdullah981066@gmail.com>
2025-07-14 03:30:59 +0530

    feat(layout): Add responsive Menubar to main layout
    This commit integrates the `Menubar` component into the main application layout, creating a responsive navigation structure.
    
    - On larger screens (sm and up), the Menubar appears as a sticky sidebar next to the main content.
    - On smaller screens, it is displayed as a sticky bottom bar for easy access on mobile devices.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import Menubar from "@/components/Menubar";
 import Navbar from "@/components/Navbar";
 import { AuthProvider } from "@/contexts/AuthContext";
 
@@ -10,7 +11,11 @@ export default function RootLayout({
     <AuthProvider>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="max-w-7xl mx-auto p-5">{children}</div>
+        <div className="max-w-7xl mx-auto p-5 flex w-full grow gap-5">
+          <Menubar className="sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80" />
+          {children}
+        </div>
+        <Menubar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </AuthProvider>
   );
